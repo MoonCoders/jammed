765c953f1fcaa743b3b5a68846e44fdf54050ee6
Kristiyan Petrov <petrov.kristiyan@gmail.com>
2020-07-04 00:23:49 +0200

    Add google maps with user location request

---
+++ app/src/main/java/com/github/mooncoders/jammed/ui/MapFragment.kt
@@ -0,0 +1,199 @@
+package com.github.mooncoders.jammed.ui
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Location
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.github.mooncoders.jammed.R
+import com.github.mooncoders.jammed.ui.foundation.BaseFragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.livinglifetechway.quickpermissions_kotlin.runWithPermissions
+import com.livinglifetechway.quickpermissions_kotlin.util.QuickPermissionsOptions
+
+
+class MapFragment : BaseFragment(), OnMapReadyCallback {
+
+    private val viewModel by lazy {
+        ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+    }
+
+    private lateinit var mMap: GoogleMap
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private var lastKnownLocation: Location? = null
+
+    private var cameraPosition: CameraPosition? = null
+
+    // The entry point to the Fused Location Provider.
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    // A default location (Milan, Italy) and default zoom to use when location permission is
+    // not granted.
+    private val defaultLocation = LatLng(45.464664, 9.188540)
+
+    // The entry point to the Places API.
+    private lateinit var placesClient: PlacesClient
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = inflater.inflate(R.layout.map_fragment, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        // Retrieve location and camera position from saved instance state.
+        if (savedInstanceState != null) {
+            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION)
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION)
+        }
+
+        // Construct a PlacesClient
+        Places.initialize(
+            requireActivity().applicationContext,
+            getString(R.string.google_maps_api_key)
+        )
+        placesClient = Places.createClient(requireActivity())
+
+        // Construct a FusedLocationProviderClient.
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        viewModel.pointsOfInterest.apply {
+            success.observe(viewLifecycleOwner, Observer { pointsOfInterest ->
+                // TODO draw on map
+            })
+
+            error.observe(viewLifecycleOwner, Observer {
+                Log.e("TAG", "Error", it)
+            })
+
+            fetch(Unit)
+        }
+
+        viewModel.pedestriansFetcher.apply {
+            success.observe(viewLifecycleOwner, Observer {
+                Log.e("TAG", "Number of pedestrians: $it")
+            })
+
+            error.observe(viewLifecycleOwner, Observer {
+                Log.e("TAG", "Error", it)
+            })
+        }
+    }
+
+    /**
+     * Saves the state of the map when the activity is paused.
+     */
+    // [START maps_current_place_on_save_instance_state]
+    override fun onSaveInstanceState(outState: Bundle) {
+        ::mMap.isInitialized.takeIf { true }?.let {
+            outState.putParcelable(KEY_CAMERA_POSITION, mMap.cameraPosition)
+            outState.putParcelable(KEY_LOCATION, lastKnownLocation)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Turn on the My Location layer and the related control on the map.
+        updateLocation()
+
+        // Add a marker in Sydney and move the camera
+//        val milanMarker = LatLng(45.464664, 9.188540)
+//        mMap.addMarker(MarkerOptions().position(milanMarker).title("Marker in Milan"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(milanMarker))
+    }
+
+    @SuppressLint("MissingPermission")
+    val mapOptions = QuickPermissionsOptions(
+        handleRationale = false,
+        rationaleMessage = "Custom rational message",
+        permanentDeniedMethod = { req ->
+            mMap.isMyLocationEnabled = false
+            mMap.uiSettings?.isMyLocationButtonEnabled = false
+            lastKnownLocation = null
+
+            mMap.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    defaultLocation,
+                    DEFAULT_ZOOM.toFloat()
+                )
+            )
+        }
+    )
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocation() = runWithPermissions(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        options = mapOptions
+    ) {
+        mMap.isMyLocationEnabled = true
+        mMap.uiSettings?.isMyLocationButtonEnabled = true
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+            task.takeIf { it.isSuccessful }?.result ?: kotlin.run {
+                mMap.moveCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        defaultLocation,
+                        DEFAULT_ZOOM.toFloat()
+                    )
+                )
+                mMap.uiSettings?.isMyLocationButtonEnabled = false
+                return@addOnCompleteListener
+            }
+
+            // Set the map's camera position to the current location of the device.
+            lastKnownLocation = task.result
+
+            mMap.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    LatLng(lastKnownLocation!!.latitude, lastKnownLocation!!.longitude),
+                    DEFAULT_ZOOM.toFloat()
+                )
+            )
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_ZOOM = 15
+
+        // Keys for storing activity state.
+        private const val KEY_CAMERA_POSITION = "camera_position"
+        private const val KEY_LOCATION = "location"
+    }
+}
--- app/src/main/java/com/github/mooncoders/jammed/ui/foundation/BaseFragment.kt
+++ app/src/main/java/com/github/mooncoders/jammed/ui/foundation/BaseFragment.kt
@@ -1,39 +1,5 @@
 package com.github.mooncoders.jammed.ui.foundation
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.github.mooncoders.jammed.ui.MainActivity
-import com.github.mooncoders.jammed.R
-import kotlinx.android.synthetic.main.fragment_base.*
-import net.gotev.recycleradapter.RecyclerAdapter
-import net.gotev.recycleradapter.ext.RecyclerAdapterProvider
 
-open class BaseFragment : Fragment(), RecyclerAdapterProvider {
-    override val recyclerAdapter by lazy { RecyclerAdapter() }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = inflater.inflate(R.layout.fragment_base, container, false)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        recycler_view.apply {
-            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
-            adapter = recyclerAdapter
-            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
-        }
-    }
-
-    fun setActionBarTitleAndSubtitle(title: String, subtitle: String? = null) {
-        (activity as? MainActivity)?.setTitleAndSubtitle(title, subtitle)
-    }
-}
+abstract class BaseFragment : Fragment()
