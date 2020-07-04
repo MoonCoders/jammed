5cd64a794d0f7be9ecdb8a780caa9e14d87454bc
Kristiyan Petrov <petrov.kristiyan@gmail.com>
2020-07-04 17:58:35 +0200

    add bottomsheet

--- app/src/main/java/com/github/mooncoders/jammed/ui/MainActivity.kt
+++ app/src/main/java/com/github/mooncoders/jammed/ui/MainActivity.kt
@@ -1,24 +1,44 @@
 package com.github.mooncoders.jammed.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.github.mooncoders.jammed.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.place_info_sheet.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<*>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) = Unit
+            override fun onStateChanged(bottomSheet: View, newState: Int) = Unit
+        })
 
-//        setSupportActionBar(toolbar)
-//        supportActionBar?.apply {
-//            setDisplayHomeAsUpEnabled(true)
-//            setDisplayShowHomeEnabled(true)
-//        }
+        bottom_nav_bar.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.explore -> {
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+                }
+                R.id.saved -> {
+                }
+                R.id.places -> {
+                }
+                R.id.more -> {
+
+                }
+            }
+            true
+        }
 
 //        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
     }
@@ -27,10 +47,4 @@ class MainActivity : AppCompatActivity() {
         return findNavController(R.id.nav_host_fragment).navigateUp()
     }
 
-    fun setTitleAndSubtitle(newTitle: String, newSubtitle: String?) {
-        supportActionBar?.apply {
-            title = newTitle
-            subtitle = newSubtitle
-        }
-    }
 }
